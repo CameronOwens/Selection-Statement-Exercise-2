ea83ac8d3d3be61216f4408172b9046b33ec4963
CameronOwens <camowens58@gmail.com>
2022-12-09 15:36:39 -0500

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math can be tough.");
+                    break;
+                case "English":
+                    Console.WriteLine("English is a great subject!");
+                    break;
+                case "History":
+                    Console.WriteLine("History is interesting.");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science can be fun.");
+                    break;
+                case "Spanish":
+                    Console.WriteLine("Spanish is muy bueno!");
+                    break;
+                default:
+                    Console.WriteLine($"That's interesting. I'm not familiar with that subject. {subject} seems like fun.");
+                    break;
+                    
+                 
+            }
+
         }
     }
 }
